c23d6eb7f2bccf8bf155ee75c80a0172ae291ff5
tysonlander <tysonlander@gmail.com>
2019-06-20 17:09:13 -0600

    initial app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import Post from './Components/Post/Post'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Form from './Components/Form/Form'
+import Nav from './Components/Nav/Nav'
+import Auth from './Components/Auth/Auth'
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
+      <Auth/>
+      <Dashboard/>
+      <Form/>
+      <Post/>
     </div>
   );
 }
